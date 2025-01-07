ad04f5759e4186cb9e8a598a7212ac0e40b230ba
donggyu <use02012@gmail.com>
2025-01-07 15:55:44 +0900

    feat: Todo Repository 세팅
    - JPA 활용을 위해 JpaRepository를 extends 하는 TodoRepository 세팅

--- src/main/java/com/ssafy/ssafysandboxbackend/crud/repository/TodoRepository.java
+++ src/main/java/com/ssafy/ssafysandboxbackend/crud/repository/TodoRepository.java
@@ -1,4 +1,9 @@
 package com.ssafy.ssafysandboxbackend.crud.repository;
 
-public interface TodoRepository{
+import com.ssafy.ssafysandboxbackend.crud.repository.domain.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
