9a9f76e6b73484a73b71dba714e0e6ee29417709
donggyu <use02012@gmail.com>
2025-01-07 15:10:43 +0900

    chore: spring boot 초기 세팅

---
+++ src/test/java/com/ssafy/ssafysandboxbackend/SsafysandboxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssafysandboxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafysandboxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
