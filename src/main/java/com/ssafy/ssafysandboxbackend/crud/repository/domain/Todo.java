e675cbdf9e8826c185d1f804a63772260c85a97c
donggyu <use02012@gmail.com>
2025-01-07 15:53:13 +0900

    feat: Todo Entity 생성
    -API 명세에 따라 Todo Entity 생성

--- src/main/java/com/ssafy/ssafysandboxbackend/crud/repository/domain/Todo.java
+++ src/main/java/com/ssafy/ssafysandboxbackend/crud/repository/domain/Todo.java
@@ -1,4 +1,19 @@
 package com.ssafy.ssafysandboxbackend.crud.repository.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 1000, nullable = false)
+    private String content;
+    @Column(nullable = false)
+    private boolean completed;
 }
